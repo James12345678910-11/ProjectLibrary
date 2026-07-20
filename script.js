6a02bceb2210942afd7a65b6b5c9755fec9fe2fb
James Calderon <jamescalderon122@gmail.com>
2026-07-20 23:16:12 +0800

    added a static display list of books available

--- script.js
+++ script.js
@@ -52,3 +52,40 @@ function removeBookFromLibrary(bookId){
 Book.prototype.toggleRead = function() {
   this.read = !this.read;
 }
+
+
+function displayBooks() {
+  container.innerHTML = '';
+  myLibrary.forEach(book => {
+    const bookCard = document.createElement("div");
+      bookCard.classList.add("book-card");
+      title.textContent = book.title;
+    const heading = document.createElement('h3');
+      heading.textContent = book.title;
+    const author = document.createElement('p');
+      author.textContent = `Author: ${book.author}`;
+    const pages = document.createElement('p');
+      pages.textContent = `Pages: ${book.pages}`;
+    const read = document.createElement('p');
+      read.textContent = `Read: ${formatBoolean(book.read)}`;
+    const addButton = document.createElement('button');
+      addButton.classList.add('add-button');
+      addButton.textContent = 'Add Book';
+    const updateButton = document.createElement('button');
+      updateButton.classList.add('update-button');
+      updateButton.textContent = 'Update Book';
+    const deleteButton = document.createElement('button');
+      deleteButton.classList.add('delete-button');
+      deleteButton.textContent = 'Delete Book';
+
+    bookCard.appendChild(heading);
+    bookCard.appendChild(title);
+    bookCard.appendChild(author);
+    bookCard.appendChild(pages);
+    bookCard.appendChild(read);
+    bookCard.appendChild(addButton);
+    bookCard.appendChild(updateButton);
+    bookCard.appendChild(deleteButton);
+    container.appendChild(bookCard); 
+  });
+}
