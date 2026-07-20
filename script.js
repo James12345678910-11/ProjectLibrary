5f9e5a55e5ca7d798e380a405b058d09bbc3319e
James Calderon <jamescalderon122@gmail.com>
2026-07-21 06:10:07 +0800

    fixed display books not showing

--- script.js
+++ script.js
@@ -1,18 +1,17 @@
 const myLibrary = [];
+const container = document.getElementById('Booklist');
 
 
 function formatBoolean(value) {
   return value ? "Yes" : "No";
-}
+} //will remove this shit. its not needed
 
 
-function Book(title, author, pages, read) {
+class BookAdd {
 
-  if ( typeof pages !== 'number' ) {
-    throw new Error("Pages must be a number")
-  }
+  constructor(title, author, pages, read) {
   if ( typeof read !== 'boolean' ) {
-    throw new Error("Read must be a yes or no")
+  throw new Error("Read must be a true or false")
   }
 
   if (typeof title !== 'string') {
@@ -25,33 +24,37 @@ function Book(title, author, pages, read) {
   if (typeof pages !== 'number' || pages <= 0) {
     throw new Error("Pages must be a positive number")
   }
+
+
   this.id = crypto.randomUUID(); 
   this.title = title;
   this.author = author;
   this.pages = pages;
   this.read = read;
+  }
+
+  static addBookToLibrary(title, author, pages, read) {
+    let book = new BookAdd(title, author, pages, read);
+    myLibrary.push(book);
+    }
 }
 
-Book.prototype.info = function() {
+BookAdd.prototype.info = function() {
   return `${this.title} by ${this.author}, ${this.pages} pages, Read: ${formatBoolean(this.read)}`;
-}
-
-function addBookToLibrary(book) {
-  const newBook = new Book(book.title, book.author, book.pages, book.read);
-  myLibrary.push(newBook);
-  saveLibrary();
 }
 
 function removeBookFromLibrary(bookId){
   const index = myLibrary.findIndex(book => book.id === bookId);
   if (index !== -1) {
     myLibrary.splice(index, 1);
+    displayBooks();
   }
 }
 
-Book.prototype.toggleRead = function() {
+BookAdd.prototype.toggleRead = function() {
   this.read = !this.read;
 }
+  
 
 
 function displayBooks() {
@@ -59,9 +62,8 @@ function displayBooks() {
   myLibrary.forEach(book => {
     const bookCard = document.createElement("div");
       bookCard.classList.add("book-card");
+    const title = document.createElement('h3');
       title.textContent = book.title;
-    const heading = document.createElement('h3');
-      heading.textContent = book.title;
     const author = document.createElement('p');
       author.textContent = `Author: ${book.author}`;
     const pages = document.createElement('p');
@@ -78,14 +80,20 @@ function displayBooks() {
       deleteButton.classList.add('delete-button');
       deleteButton.textContent = 'Delete Book';
 
-    bookCard.appendChild(heading);
     bookCard.appendChild(title);
     bookCard.appendChild(author);
     bookCard.appendChild(pages);
     bookCard.appendChild(read);
-    bookCard.appendChild(addButton);
     bookCard.appendChild(updateButton);
     bookCard.appendChild(deleteButton);
     container.appendChild(bookCard); 
   });
 }
+
+
+BookAdd.addBookToLibrary("The Fellowship of the Ring", "JRR Tolkien", 576, true);
+BookAdd.addBookToLibrary("Harry Potter and the Philosophers Stone", "JK Rowling", 352, true);
+BookAdd.addBookToLibrary("Blackflame", "Will Wight", 369, true);
+BookAdd.addBookToLibrary("Mistborn", "Brandon Sanderson", 672, true);
+BookAdd.addBookToLibrary("A Knight of the Seven Kingdoms", "George RR Martin", 368, true);
+displayBooks();
